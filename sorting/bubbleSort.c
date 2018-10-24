208f279f13497ffe429d3503c355829e698fb54f
Augustine S Aykara <augustineaykara@gmail.com>
2018-10-24 21:23:33 +0530

    Add files via upload

---
+++ sorting/bubbleSort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+
+void selectionSort(int arr[], int s)
+{
+	int i, j, temp;
+
+	for (i=0; i<s; ++i)
+	{
+		for (j=0; j<s-i-1; ++j)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+
+	printf("\n After sorting...\n");
+	for (i=0; i<s; ++i)
+	{
+		printf(" %d ", arr[i]);
+	}
+	printf("\n");
+}
+
+
+void main()
+{
+	int arr[10], size, i ,j;
+
+	printf("\n Enter the size of array : ");
+	scanf("%d", &size);
+
+	printf("\n Enter the array elements : ");
+	for(i=0; i<size; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	selectionSort(arr, size);
+}
